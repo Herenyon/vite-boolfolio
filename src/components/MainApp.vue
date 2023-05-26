326aac3d655fbfda174fe1d0d9c7b59e9ef55e9a
Herenyon <rikitur17@gmail.com>
2023-05-26 20:57:17 +0200

    esercizio finito

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
     name: 'Main',
+    data() {
+        return {
+            imagePath: 'http://127.0.0.1:8000/storage/'
+        }
+    },
     props: {
         data: Object
     }
@@ -12,10 +17,10 @@ export default {
         <h1>Main</h1>
         <div class="col col-md-4" v-for="portf in data">
             <div class="card my-4 gy-4">
-                <img src="..." class="card-img-top" alt="...">
+                <img v-if="portf.image" :src="this.imagePath + portf.image" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">{{ portf.repo_title }}</h5>
-                    <p class="card-text">{{ portf.description }}</p>
+                    <p class="card-text">{{ portf.description.substring(0, 100) }}</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
